b921710307d0051e6e335d6cf78c02de78e77882
Ying <zhangwei9046@gmail.com>
2016-01-23 14:59:07 -0800

    Add public static page

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var app = express();
+
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
 app.get('/hello', function(req, res){
-    res.send('hello world');
+    res.send('Hello world!');
 });
+
 app.listen(port, ipaddress);
