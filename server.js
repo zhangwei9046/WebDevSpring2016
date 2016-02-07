c8ea9e48ff07eac10e46d7fca2a83d57b0a22bf7
Ying <zhangwei9046@gmail.com>
2016-02-06 18:10:37 -0800

    change title

--- server.js
+++ server.js
@@ -1,13 +1,6 @@
 var express = require('express');
 var app = express();
-
+app.use(express.static(__dirname + '/public'));
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
-
-app.use(express.static(__dirname + '/public'));
-
-app.get('/hello', function(req, res){
-    res.send('Hello world!');
-});
-
 app.listen(port, ipaddress);
